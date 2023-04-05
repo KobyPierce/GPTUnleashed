8aca5378e891fc3c6942ae02185a3c286aa4d333
Koby Pierce <kobypierce@hybridhosting.xyz>
2023-04-05 19:48:10 -0400

    API Test Push +2

--- chat/script.js
+++ chat/script.js
@@ -1,30 +1,68 @@
-// Center the model selector horizontally
-const modelSelector = document.getElementById("model-selector");
-const sidebarWidth = document.getElementById("sidebar").offsetWidth;
-modelSelector.style.left = (sidebarWidth + (window.innerWidth - sidebarWidth) / 2 - modelSelector.offsetWidth / 2) + "px";
+// Model selector
+const modelSelect = document.getElementById("modelSelect");
+const params = new URLSearchParams(window.location.search);
+const model = params.get("model");
 
-// Update the model selector position on window resize
-window.addEventListener("resize", function () {
-  modelSelector.style.left = (sidebarWidth + (window.innerWidth - sidebarWidth) / 2 - modelSelector.offsetWidth / 2) + "px";
-});
+if (model) {
+  modelSelect.value = model;
+}
 
-// Form submission handler
-document.getElementById("submit").addEventListener("click", function (event) {
-  event.preventDefault(); // Prevent form submission
+function updateURL() {
+  const newUrl = new URL(window.location);
+  newUrl.searchParams.set("model", modelSelect.value);
+  window.location.href = newUrl;
+}
 
-  const model = document.getElementById("model-selector").value;
-  const content = document.getElementById("search-bar").value;
+// Search bar
+const searchForm = document.querySelector(".search-form");
+const searchInput = document.querySelector("#search-input");
+const searchResults = document.querySelector(".search-results");
 
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", "/api/gpt", true);
-  xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      const response = JSON.parse(xhr.responseText);
-      const result = document.createElement("p");
-      result.innerHTML = response.response;
-      document.body.appendChild(result);
-    }
+function submitForm(event) {
+  event.preventDefault();
+  const query = searchInput.value;
+  if (query.trim() === "") return;
+  const model = modelSelect.value;
+  const messages = [{ role: "user", content: query }];
+  if (model) messages.push({ role: "system", content: `Using ${model} model` });
+  searchInput.value = "";
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      model: model,
+      messages: messages,
+    }),
   };
-  xhr.send(JSON.stringify({ model: model, content: content }));
-});
+  fetch("https://api.openai.com/v1/engines/davinci-codex/completions", requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      const answer = data.choices[0].text.trim();
+      const result = `<p><strong>You:</strong> ${query}</p>
+                      <p><strong>GPT:</strong> ${answer}</p>`;
+      searchResults.innerHTML = result;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      const result = `<p>Error: ${error.message}</p>`;
+      searchResults.innerHTML = result;
+    });
+}
+
+// Centered text
+const centeredText = document.querySelector(".centered-text");
+centeredText.style.opacity = "0.25";
+centeredText.style.textAlign = "center";
+centeredText.style.marginTop = "25%";
+
+// Model dropdown
+const modelDropdown = document.querySelector(".model-dropdown");
+modelDropdown.style.textAlign = "center";
+modelDropdown.style.marginBottom = "20px";
+
+// Search bar and results
+const searchWrapper = document.querySelector(".search-wrapper");
+searchWrapper.style.textAlign = "center";
+searchWrapper.style.marginBottom = "40px";
+
+searchForm.addEventListener("submit", submitForm);
